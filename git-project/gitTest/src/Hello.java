ea2600f6b2b2c0ae837f2b567ad459a0d2409fe3
yaoyu <abcyaoyu@163.com>
2021-02-05 17:13:42 +0800

    branch1分支添加sex方法

--- git-project/gitTest/src/Hello.java
+++ git-project/gitTest/src/Hello.java
@@ -1,9 +1,17 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        sex();
     }
 
     public int add(int a, int b) {
         return a + b;
+    }
+
+    public static boolean sex(){
+        if ('a' == 97){
+            return true;
+        }
+        return false;
     }
 }
