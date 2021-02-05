a9e1811d54dc79a1831e0457eeddd9676f6bc3cf
yaoyu <abcyaoyu@163.com>
2021-02-05 17:10:39 +0800

    添加add方法

--- git-project/gitTest/src/Hello.java
+++ git-project/gitTest/src/Hello.java
@@ -2,4 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git!");
     }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
 }
